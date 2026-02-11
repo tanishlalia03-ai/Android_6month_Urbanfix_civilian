2460924a4727ad43bebbc129b5a318fd51d94d2c
tanishlalia03-ai <tanishlalia03@gmail.com>
2026-02-11 11:20:57 +0530

    work

--- app/src/main/java/com/example/urbanfix/Splashscreen/SplashscreenActivity.kt
+++ app/src/main/java/com/example/urbanfix/Splashscreen/SplashscreenActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.urbanfix.MainActivity
 import com.example.urbanfix.R
